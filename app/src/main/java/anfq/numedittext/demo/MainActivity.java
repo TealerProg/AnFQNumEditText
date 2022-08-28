2946994d2ce51055a7f57115f9e100ec51a8d160
TealerProg <1162947801@qq.com>
2022-08-28 10:41:27 +0800

    配置信息

--- app/src/main/java/anfq/numedittext/demo/MainActivity.java
+++ app/src/main/java/anfq/numedittext/demo/MainActivity.java
@@ -1,7 +1,8 @@
 package anfq.numedittext.demo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import anfq.numedittext.lib.AnFQNumEditText;
 
